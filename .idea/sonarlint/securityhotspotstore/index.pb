4ec39c34477899eab9e9c9fcd64a4c545e527317
Amund F. P. Larsen <amund.f.p.larsen@gmail.com>
2024-02-27 11:56:14 +0100

    household test

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,7 @@ _
 /src/main/java/ntnu/org/IDATG1005/grp3/Main.java,1\d\1d08d15a730f0dd4060baacd04ab6f5b83728657
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+s
+Csrc/main/java/ntnu/org/IDATG1005/grp3/service/HouseholdService.java,c\2\c2d539af619b41f615a3eea0e125d62c2aaf1d0e
+j
+:src/main/java/ntnu/org/IDATG1005/grp3/model/Household.java,4\9\49e719448653697a4dbf2366409686fca7c4a165
--- src/main/java/ntnu/org/IDATG1005/grp3/Main.java
+++ src/main/java/ntnu/org/IDATG1005/grp3/Main.java
@@ -1,11 +1,14 @@
 package ntnu.org.IDATG1005.grp3;
 
+import ntnu.org.IDATG1005.grp3.model.Household;
+import ntnu.org.IDATG1005.grp3.service.HouseholdService;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
+    Household household = new Household("Amund", "A");
+    HouseholdService householdService = new HouseholdService();
+    householdService.createHousehold(household.getName(), household.getJoinCode());
+    householdService.findHouseholdByJoinCode("A");
   }
 }
