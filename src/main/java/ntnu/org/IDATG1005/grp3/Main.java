7991b2d15f20aa85eadba4e29e791508bf1d85ea
elling <elling.bakaas@gmail.com>
2024-02-27 10:51:49 +0100

    Test

--- src/main/java/ntnu/org/IDATG1005/grp3/Main.java
+++ src/main/java/ntnu/org/IDATG1005/grp3/Main.java
@@ -1,8 +1,22 @@
 package ntnu.org.IDATG1005.grp3;
 
+import java.security.Provider;
+import java.security.Provider.Service;
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    Provider provider = new Provider("Provider 1", 1.0, "Info 1") {
+      @Override
+      public Service getService(String type, String algorithm) {
+        return super.getService(type, algorithm);
+      }
+    };
+    Service service = new Service(provider, "Type 1", "Algorithm 1", "ClassName 1", null, null);
+    System.out.println(service.getProvider().getInfo());
+
+
+
   }
 }
