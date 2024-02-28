472cfb7ab96f48c87ab46ea74d1fe13bc0e045e8
Oskar Kjos <oskarkjo@stud.ntnu.no>
2024-02-28 15:47:30 +0100

    Added javaFX and FXML

---
+++ src/main/java/ntnu/org/IDATG1005/grp3/application/TheFoodPlannerApp.java
@@ -0,0 +1,22 @@
+package ntnu.org.IDATG1005.grp3.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TheFoodPlannerApp extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("/fxmls/aSimpleTest.fxml"));
+    primaryStage.setTitle("Application Title");
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
