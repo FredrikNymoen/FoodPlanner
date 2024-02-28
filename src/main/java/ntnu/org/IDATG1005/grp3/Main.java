8be7c94fe8829a79af9919ebfba887705aec79e6
Amund F. P. Larsen <amund.f.p.larsen@gmail.com>
2024-02-28 16:27:56 +0100

    tried to enable javafx

--- src/main/java/ntnu/org/IDATG1005/grp3/Main.java
+++ src/main/java/ntnu/org/IDATG1005/grp3/Main.java
@@ -3,12 +3,24 @@ package ntnu.org.IDATG1005.grp3;
 import ntnu.org.IDATG1005.grp3.model.Household;
 import ntnu.org.IDATG1005.grp3.service.HouseholdService;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-start.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    stage.setTitle("Hello!");
+    stage.setScene(scene);
+    stage.show();
+  }
 
   public static void main(String[] args) {
-    Household household = new Household("Amund", "A");
-    HouseholdService householdService = new HouseholdService();
-    householdService.createHousehold(household.getName(), household.getJoinCode());
-    householdService.findHouseholdByJoinCode("A");
+    launch();
   }
 }
