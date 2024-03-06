2ea9ad00a88aa8654fb9918f96b5826cae2d41ef
Oskar Kjos <oskarkjo@stud.ntnu.no>
2024-03-06 17:19:55 +0100

    Added Javadoc, and made immutable

--- src/main/java/ntnu/org/IDATG1005/grp3/model/objects/Ingredient.java
+++ src/main/java/ntnu/org/IDATG1005/grp3/model/objects/Ingredient.java
@@ -1,36 +1,50 @@
 package ntnu.org.IDATG1005.grp3.model.objects;
 
 /**
- * Represents an ingredient with a unique ID, name, and image URL.
+ * Represents a base ingredient with a unique ID, name, and image URL.
  */
 public class Ingredient {
   private final Integer ingredientId;
-  private String name;
-  private String imageUrl;
+  private final String name;
+  private final String imageUrl;
 
+  /**
+   * Constructs an Ingredient with specified ID, name, and image URL.
+   *
+   * @param ingredientId The unique identifier for the ingredient.
+   * @param name The name of the ingredient.
+   * @param imageUrl The URL of the image associated with the ingredient.
+   */
   public Ingredient(Integer ingredientId, String name, String imageUrl) {
     this.ingredientId = ingredientId;
     this.name = name;
     this.imageUrl = imageUrl;
   }
 
+  /**
+   * Gets the ingredient's unique identifier.
+   *
+   * @return The ingredient's ID.
+   */
   public Integer getIngredientId() {
     return ingredientId;
   }
 
+  /**
+   * Gets the ingredient's name.
+   *
+   * @return The name of the ingredient.
+   */
   public String getName() {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
+  /**
+   * Gets the URL of the ingredient's image.
+   *
+   * @return The image URL associated with the ingredient.
+   */
   public String getImageUrl() {
     return imageUrl;
-  }
-
-  public void setImageUrl(String imageUrl) {
-    this.imageUrl = imageUrl;
   }
 }
