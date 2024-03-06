95ce414aac054d98ff6c4a0d7b57798594289143
Oskar Kjos <oskarkjo@stud.ntnu.no>
2024-03-06 13:36:03 +0100

    Fixed major bug

--- src/main/java/ntnu/org/IDATG1005/grp3/model/objects/InventoryIngredient.java
+++ src/main/java/ntnu/org/IDATG1005/grp3/model/objects/InventoryIngredient.java
@@ -1,20 +1,52 @@
 package ntnu.org.IDATG1005.grp3.model.objects;
 
-import ntnu.org.IDATG1005.grp3.model.objects.Ingredient;
-
 /**
  * Represents an ingredient in the inventory, including quantity and unit.
  */
 public class InventoryIngredient {
   private final Ingredient ingredient;
-
-  // unit here
+  private final Unit unit;
   private Integer quantity;
 
-  public InventoryIngredient(Integer inventoryIngredientId, Integer inventoryId, Integer ingredientId, Integer unitId, Double quantity) {
-    this.unitId = unitId;
+  public InventoryIngredient(Ingredient ingredient, Unit unit, Integer quantity) {
+    this.ingredient = ingredient;
+    this.unit = unit;
     this.quantity = quantity;
   }
 
-  // Getters and setters...
+  /**
+   * Gets the ingredient.
+   *
+   * @return The ingredient.
+   */
+  public Ingredient getIngredient() {
+    return ingredient;
+  }
+
+  /**
+   * Gets the unit of measurement for the ingredient.
+   *
+   * @return The unit.
+   */
+  public Unit getUnit() {
+    return unit;
+  }
+
+  /**
+   * Gets the quantity of the ingredient.
+   *
+   * @return The quantity.
+   */
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  /**
+   * Sets the quantity of the ingredient.
+   *
+   * @param quantity The new quantity.
+   */
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
 }
